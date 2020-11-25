6fb8895efcc2cf2e6022162533f6557a3f9df25a
alligator <enemy.forest.brigade@gmail.com>
2020-11-25 17:56:49 +0000

    add react

--- FullStackTest/ClientApp/webpack.config.js
+++ FullStackTest/ClientApp/webpack.config.js
@@ -22,6 +22,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /js|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
         test: /\.(scss)$/,
         use: [
           {
