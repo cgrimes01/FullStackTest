74b20fa4b182ce1bb777d2482cda4debdf1dd2a0
Ciaran Grimes <chfgrimes@gmail.com>
2020-12-09 07:56:16 +0000

    Break out all Statistics calculations into separate files

---
+++ FullStackTest/Services/Statistics/MedianCalculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FullStackTest.Services.Statistics
+{
+    public static partial class Calculations
+    {
+        public static float Median(int[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
---
+++ FullStackTest/Services/Statistics/ModeCalculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FullStackTest.Services.Statistics
+{
+    public static partial class Calculations
+    {
+        public static int[] Mode(int[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
