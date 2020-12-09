800f1451fb1537c89cc5f73d84b9389a948d499a
Ciaran Grimes <chfgrimes@gmail.com>
2020-12-09 08:41:13 +0000

    Add Range as a possible statistics calculation, with a broken implementation

---
+++ FullStackTest/Services/Statistics/RangeCalculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FullStackTest.Services.Statistics
+{
+    public static partial class Calculations
+    {
+        public static long Range(int[] inputs)
+        {
+            /*
+                Return the difference between the largest and smallest number in a given set. 
+            */
+            int minimum = 0;
+            int maximum = 0;
+            foreach (int i in inputs)
+            {
+                if (i < minimum)
+                    minimum = i;
+                else if (i > maximum)
+                    maximum = i;
+            }
+            return maximum - minimum;
+        }
+    } 
+}
