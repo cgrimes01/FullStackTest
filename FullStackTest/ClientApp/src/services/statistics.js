a64b204a5675919fc32a68b502fc69399fd53860
Ciaran Grimes <chfgrimes@gmail.com>
2021-01-03 18:22:45 +0000

    Add in stubbed out fibonacci service

---
+++ FullStackTest/ClientApp/src/services/statistics.js
@@ -0,0 +1,6 @@
+export const requestStatistics = {
+    fibonaci: (sequence) => {
+      const url = `calculator/fibonacci?input=${sequence}`
+      return fetch(url).then(response => response.text());
+    },
+};
